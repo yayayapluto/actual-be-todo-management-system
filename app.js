d0e4f366d293e7db4a6b0ce848bad3f3eb11ac03
LumiNovryM <luminovrymekel@gmail.com>
2025-01-25 09:33:55 +0700

    chore: setup project

---
+++ app.js
@@ -0,0 +1,54 @@
+// Import Module & Declare Variable
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+// Import DB Connection
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+// Create Express App
+var app = express(); 
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define Route
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Handle Error
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Set port
+const port = process.env.PORT || 4000;
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+const { pool } = require('../database/db');
+
+
+// GET all users
+router.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err.message);
+  }
+});
+
+module.exports = router;    
