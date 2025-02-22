f2464fc70b06a924c2567dc3eceb5b822d8647ef
yyypluto <mfarrasashshiddiqi@gmail.com>
2025-02-22 12:16:25 +0700

    fix: remove jade view

--- index.js
+++ index.js
@@ -18,10 +18,6 @@ var userTaskRouter = require('./routes/userTask');
 // Create Express App
 var app = express(); 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
